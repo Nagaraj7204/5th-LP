d90340b8b7f8a22a05e4702ad2b89feef1402b65
Nagaraj7204 <nagaraj7204343@gmail.com>
2025-05-13 15:34:02 +0530

    FIB

---
+++ Fib.java
@@ -0,0 +1,14 @@
+public class Fib {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0,b = 1;
+        System.out.println("Fibonacci Series upto"+n+"terms:");
+        for (int i=1;i<=n;++i){
+            System.out.println(a+" ");
+            int next = a+b;
+            a=b;
+            b=next;
+        }
+    }
+    
+}
